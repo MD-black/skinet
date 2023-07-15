267d6f43328f1cf4054c8040029833e3545eb09c
MD-black <mhd.darwish1@gmail.com>
2023-07-15 19:31:47 +0300

    end of section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -28,6 +28,10 @@ export class ShopService {
     return this.http.get<Pagination<Product[]>>(this.baseUrl + this.productsUrl, { params });
   }
 
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + this.productsUrl + `/${id}`);
+  }
+
   getBrands() {
     return this.http.get<Brand[]>(this.baseUrl + this.brandsUrl);
   }
